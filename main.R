a44f331cfead59433fc299cb1c3a4ac9b7553780
Naidu <rongalinaidu@utexas.com>
2023-03-09 11:20:04 -0800

    added some examples from todays class

--- main.R
+++ main.R
@@ -1,7 +1,60 @@
-5 + 5
-"Hello, Naidu"
-print("Hello, Naidu")
+#variable
 
-for (x in 1:10) {
-  print(x)
-}
+x <- 123
+x + 5
+
+name <- "Naidu"
+#printing variable
+name
+
+#concatenating two strings
+paste("Hello, ",name)
+
+#for (x in 1:10) {
+#  print(x)
+#}
+
+#
+#> sqrt(100)
+#[1] 10
+#> sqrt(10)
+#[1] 3.162278
+#> log(10)
+#[1] 2.302585
+#> factorial(10)
+#[1] 3628800
+#> log(factorial(10))
+#[1] 15.10441
+#> choose(15,5)
+#[1] 3003
+#> factorial(15)/(factorial(5)*factorial(10))
+#[1] 3003
+#> factorial(100)
+#[1] 9.332622e+157
+#> factorial(1000)
+#[1] Inf
+#> choose(1500,2)
+#[1] 1124250
+#> factorial(1500)/(factorial(2)*factorial(1498))
+#[1] NaN
+
+
+#Ctrl + L is shortcut for clearning the screen
+
+
+#> x <- 5
+#> y <- 2
+#> choose(x,y)
+#[1] 10
+#> x+y
+#[1] 7
+#> `+`(x,y)
+#[1] 7
+#> choose
+#function (n, k)
+#.Internal(choose(n, k))
+#<bytecode: 0x563e398b5e50>
+#<environment: namespace:base>
+#> str(choose)
+#function (n, k)
+
